d18c08a18da09c774cdcbc9d8c871178df03febc
Nam <namdeveloper.ca@gmail.com>
2024-03-02 18:50:19 +0700

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.get('/api/test', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from server!' })
+})
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`)
+})
