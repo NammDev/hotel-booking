6bda20cdf290c9f5c0d1e48f53389f7ba2b42397
Nam <namdeveloper.ca@gmail.com>
2024-03-06 18:04:41 +0700

    ui for register page with email & password

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -2,8 +2,15 @@ import Link from 'next/link'
 
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
-import { Icons } from '@/components/icons'
-import { UserAuthForm } from '@/components/user-auth-form'
+import { SignUpForm } from '@/components/forms/signup-form'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 export const metadata = {
   title: 'Create an account',
@@ -28,28 +35,37 @@ export default function RegisterPage() {
         }}
         className='bg-cover hidden h-full lg:block bg-gradient-to-t from-background to-background/60 md:to-background/40'
       />
-      <div className='lg:p-8'>
-        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
-          <div className='flex flex-col space-y-2 text-center'>
-            <Icons.logo className='mx-auto h-6 w-6' />
-            <h1 className='text-2xl font-semibold tracking-tight'>Create an account</h1>
-            <p className='text-sm text-muted-foreground'>
-              Enter your email below to create your account
-            </p>
-          </div>
-          <UserAuthForm />
-          <p className='px-8 text-center text-sm text-muted-foreground'>
-            By clicking continue, you agree to our{' '}
-            <Link href='/terms' className='hover:text-brand underline underline-offset-4'>
-              Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link href='/privacy' className='hover:text-brand underline underline-offset-4'>
-              Privacy Policy
-            </Link>
-            .
-          </p>
-        </div>
+      <div className='grid items-center gap-8 pb-8 pt-6 md:py-8 container max-w-lg'>
+        <Card>
+          <CardHeader className='space-y-1'>
+            <CardTitle className='text-2xl'>Sign up</CardTitle>
+            <CardDescription>Choose your preferred sign up method</CardDescription>
+          </CardHeader>
+          <CardContent className='grid gap-4'>
+            {/* <OAuthSignIn /> */}
+            <div className='relative'>
+              <div className='absolute inset-0 flex items-center'>
+                <span className='w-full border-t' />
+              </div>
+              <div className='relative flex justify-center text-xs uppercase'>
+                <span className='bg-background px-2 text-muted-foreground'>Or continue with</span>
+              </div>
+            </div>
+            <SignUpForm />
+          </CardContent>
+          <CardFooter>
+            <div className='text-sm text-muted-foreground'>
+              Already have an account?{' '}
+              <Link
+                aria-label='Sign in'
+                href='/login'
+                className='text-primary underline-offset-4 transition-colors hover:underline'
+              >
+                Sign in
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   )
