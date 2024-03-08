d2302e7bfea21e318efc7bf69f2288a6c317f610
Nam <namdeveloper.ca@gmail.com>
2024-03-08 11:57:37 +0700

    redesgin avatar for header

--- frontend/components/icons.tsx
+++ frontend/components/icons.tsx
@@ -25,6 +25,8 @@ import {
   X,
 } from 'lucide-react'
 
+type IconProps = React.HTMLAttributes<SVGElement>
+
 export const Icons = {
   logo: Command,
   close: X,
@@ -66,4 +68,19 @@ export const Icons = {
   ),
   twitter: Twitter,
   check: Check,
+  credit: (props: IconProps) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      {...props}
+    >
+      <rect width='20' height='14' x='2' y='5' rx='2' />
+      <line x1='2' x2='22' y1='10' y2='10' />
+    </svg>
+  ),
 }
