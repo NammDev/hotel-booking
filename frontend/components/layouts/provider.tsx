00e1170d3dbc2ee010bcf252f71577e889d3297f
Nam <namdeveloper.ca@gmail.com>
2024-03-22 18:39:41 +0700

    Search Context

--- frontend/components/layouts/provider.tsx
+++ frontend/components/layouts/provider.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from 'next-themes'
 import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { SearchContextProvider } from '@/context/SearchContext'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -23,7 +24,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-        {children}
+        <SearchContextProvider>{children}</SearchContextProvider>
       </ThemeProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
