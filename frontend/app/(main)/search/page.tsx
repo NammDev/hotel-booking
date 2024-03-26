c2a536aaf9a4d9ffedce025789736ce77f9750a1
Nam <namdeveloper.ca@gmail.com>
2024-03-26 14:15:17 +0700

    wander hotel

--- frontend/app/(main)/search/page.tsx
+++ frontend/app/(main)/search/page.tsx
@@ -489,7 +489,7 @@ export default function SearchPage() {
         </div>
         <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {hotelData.data.map((hotel) => (
-            <ProductCard key={hotel._id} hotel={hotel} />
+            <ProductCard key={hotel._id} hotel={hotel} variant='wander' />
           ))}
         </div>
         {hotelData.data.length ? (
--- frontend/components/cards/product-card.tsx
+++ frontend/components/cards/product-card.tsx
@@ -16,13 +16,13 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { HotelType } from '@/lib/type'
-import { cn, formatPrice } from '@/lib/utils'
+import { cn, formatPrice, toTitleCase } from '@/lib/utils'
 import { Icons } from '../my-ui/icons'
 import { PlaceholderImage } from '../my-ui/placeholder'
 
 interface ProductCardProps extends React.HTMLAttributes<HTMLDivElement> {
   hotel: HotelType
-  variant?: 'default' | 'switchable'
+  variant?: 'default' | 'switchable' | 'wander'
   isAddedToCart?: boolean
   onSwitch?: () => Promise<void>
 }
@@ -40,13 +40,13 @@ export function ProductCard({
   return (
     <Card className={cn('size-full overflow-hidden rounded-sm', className)} {...props}>
       <Link aria-label={hotel.name} href={`/hotel/${hotel._id}`}>
-        <CardHeader className='border-b p-0'>
-          <AspectRatio ratio={4 / 3}>
+        <CardHeader className='p-0'>
+          <AspectRatio ratio={7 / 6}>
             {hotel.imageUrls?.length ? (
               <Image
                 src={hotel.imageUrls[0]}
                 alt={hotel.name}
-                className='object-cover'
+                className='object-cover rounded-sm'
                 sizes='(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw'
                 fill
                 loading='lazy'
@@ -62,7 +62,7 @@ export function ProductCard({
         <CardContent className='space-y-1.5 p-4'>
           <CardTitle className='line-clamp-1'>{hotel.name}</CardTitle>
           <CardDescription className='line-clamp-1'>
-            {formatPrice(hotel.pricePerNight)}
+            Price Per Night: {formatPrice(hotel.pricePerNight)}
           </CardDescription>
         </CardContent>
       </Link>
@@ -89,6 +89,13 @@ export function ProductCard({
               <EyeOpenIcon className='size-4' aria-hidden='true' />
               <span className='sr-only'>Preview</span>
             </Link>
+          </div>
+        ) : variant === 'wander' ? (
+          <div className='flex h-full flex-col group-[.is]/marker:px-3 group-[.is]/marker:pb-3'>
+            <div className='text-property-eyebrow text-white/60'>
+              {hotel.city}, {hotel.country}
+            </div>
+            <div className='mb-0.5 flex items-end gap-2 font-medium'>{toTitleCase(hotel.type)}</div>
           </div>
         ) : (
           <Button
