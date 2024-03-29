c34cb001c6565af55a08f5a68995f323c8986cb8
Nam <namdeveloper.ca@gmail.com>
2024-03-29 09:54:09 +0700

    Guest Info Form UI

--- frontend/components/forms/guest-info-form.tsx
+++ frontend/components/forms/guest-info-form.tsx
@@ -1,12 +1,14 @@
-import { useForm } from 'react-hook-form'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { Icons } from '../my-ui/icons'
 import { Calendar } from '../ui/calendar'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import type { DateRange } from 'react-day-picker'
-import { addDays } from 'date-fns'
+import { addDays, differenceInDays } from 'date-fns'
 import { useSearchContext } from '@/context/SearchContext'
 import NcInputNumber from '../my-ui/nc-input-number'
+import { usePathname, useRouter } from 'next/navigation'
+import { useProfile } from '@/hooks/use-profile'
+import { Button } from '../ui/button'
 
 type Props = {
   hotelId: string
@@ -22,8 +24,10 @@ type GuestInfoFormData = {
 
 const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
   const search = useSearchContext()
+  const router = useRouter()
+  const pathname = usePathname()
 
-  //   const { isLoggedIn } = useAppContext()
+  const { data: user } = useProfile()
 
   const defaultSelected: DateRange = {
     from: search.checkIn || new Date(),
@@ -31,38 +35,26 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
   }
   const [date, setDate] = useState<DateRange | undefined>(defaultSelected)
   const [adultCount, setAdultCount] = useState<number>(search.adultCount)
+  const [childCount, setChildCount] = useState<number>(search.childCount)
 
-  const {
-    watch,
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm<GuestInfoFormData>({
-    defaultValues: {
-      checkIn: search.checkIn,
-      checkOut: search.checkOut,
-      adultCount: search.adultCount,
-      childCount: search.childCount,
-    },
-  })
-
-  const checkIn = watch('checkIn')
-  const checkOut = watch('checkOut')
-
-  const minDate = new Date()
-  const maxDate = new Date()
-  maxDate.setFullYear(maxDate.getFullYear() + 1)
-
-  const onSignInClick = (data: GuestInfoFormData) => {
-    search.saveSearchValues('', data.checkIn, data.checkOut, data.adultCount, data.childCount)
-    // navigate('/sign-in', { state: { from: location } })
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    search.saveSearchValues(
+      search.destination,
+      date?.from as Date,
+      date?.to as Date,
+      adultCount,
+      childCount
+    )
+    if (user) {
+      // navigate(`/hotel/${hotelId}/booking`)
+    } else {
+      router.push(`/signin?callbackUrl=${encodeURIComponent(pathname)}`)
+    }
   }
 
-  const onSubmit = (data: GuestInfoFormData) => {
-    search.saveSearchValues('', data.checkIn, data.checkOut, data.adultCount, data.childCount)
-    // navigate(`/hotel/${hotelId}/booking`)
-  }
+  const totalGuests = adultCount + childCount
+  const totalNights = date && date.from && date.to ? differenceInDays(date.to, date.from) : 0
 
   return (
     <>
@@ -78,7 +70,7 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
       </div>
 
       {/* FORM */}
-      <form className='flex flex-col border border-neutral-200 dark:border-neutral-700 rounded-3xl '>
+      <div className='flex flex-col border border-neutral-200 dark:border-neutral-700 rounded-3xl '>
         <Popover>
           <PopoverTrigger className='flex-1 flex relative p-3 items-center space-x-3 focus:outline-none '>
             <div className='text-neutral-300 dark:text-neutral-400'>
@@ -105,7 +97,7 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
             {defaultSelected.from && (
               <Icons.close2
                 className='absolute z-10 w-5 h-5 lg:w-6 lg:h-6 text-sm bg-neutral-200 dark:bg-neutral-800 rounded-full flex items-center justify-center right-1 lg:right-3 top-1/2 transform -translate-y-1/2'
-                // onClick={() => setDate(['', ''])}
+                onClick={() => setDate(undefined)}
               />
             )}
           </PopoverTrigger>
@@ -121,7 +113,6 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
             />
           </PopoverContent>
         </Popover>
-        {/* <StayDatesRangeInput className='flex-1 z-[11]' /> */}
         <div className='w-full border-b border-neutral-200 dark:border-neutral-700'></div>
         <Popover>
           <PopoverTrigger className='relative z-10 flex-1 flex text-left items-center p-3 space-x-3 focus:outline-none'>
@@ -129,27 +120,27 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
               <Icons.userPlus className='w-5 h-5 lg:w-7 lg:h-7' />
             </div>
             <div className='flex-grow'>
-              <span className='block font-semibold xl:text-lg'>{adultCount || ''} Guests</span>
+              <span className='block font-semibold xl:text-lg'>{totalGuests || ''} Guests</span>
               <span className='block mt-1 text-sm font-light leading-none text-neutral-400'>
-                {adultCount ? 'Guests' : 'Add guests'}
+                {totalGuests ? 'Guests' : 'Add guests'}
               </span>
             </div>
 
-            {/* {!!totalGuests && open && (
-              <ClearDataButton
+            {!!totalGuests && (
+              <Icons.close2
+                className='absolute z-10 w-5 h-5 lg:w-6 lg:h-6 text-sm bg-neutral-200 dark:bg-neutral-800 rounded-full flex items-center justify-center right-1 lg:right-3 top-1/2 transform -translate-y-1/2'
                 onClick={() => {
-                  setGuestAdultsInputValue(0)
-                  setGuestChildrenInputValue(0)
-                  setGuestInfantsInputValue(0)
+                  setAdultCount(0)
+                  setChildCount(0)
                 }}
               />
-            )} */}
+            )}
           </PopoverTrigger>
           <PopoverContent className='w-full sm:min-w-[340px] max-w-sm bg-white dark:bg-neutral-800 top-full mt-3 py-5 sm:py-6 px-4 sm:px-8 rounded-3xl shadow-xl ring-1 ring-black ring-opacity-5'>
             <NcInputNumber
               className='w-full'
-              // defaultValue={guestAdultsInputValue}
-              // onChange={(value) => handleChangeData(value, 'guestAdults')}
+              defaultValue={adultCount}
+              onChange={(value) => setAdultCount(value)}
               max={10}
               min={1}
               label='Adults'
@@ -157,30 +148,23 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
             />
             <NcInputNumber
               className='w-full mt-6'
-              // defaultValue={guestChildrenInputValue}
-              // onChange={(value) => handleChangeData(value, 'guestChildren')}
+              defaultValue={childCount}
+              onChange={(value) => setChildCount(value)}
               max={4}
               label='Children'
               desc='Ages 2–12'
             />
-
-            <NcInputNumber
-              className='w-full mt-6'
-              // defaultValue={guestInfantsInputValue}
-              // onChange={(value) => handleChangeData(value, 'guestInfants')}
-              max={4}
-              label='Infants'
-              desc='Ages 0–2'
-            />
           </PopoverContent>
         </Popover>
-      </form>
+      </div>
 
       {/* SUM */}
       <div className='flex flex-col space-y-4'>
         <div className='flex justify-between text-neutral-6000 dark:text-neutral-300'>
-          <span>${pricePerNight} x 3 night</span>
-          <span>$357</span>
+          <span>
+            ${pricePerNight} x {totalNights} night
+          </span>
+          <span>${pricePerNight * totalNights}</span>
         </div>
         <div className='flex justify-between text-neutral-6000 dark:text-neutral-300'>
           <span>Service charge</span>
@@ -189,12 +173,24 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
         <div className='border-b border-neutral-200 dark:border-neutral-700'></div>
         <div className='flex justify-between font-semibold'>
           <span>Total</span>
-          <span>$199</span>
+          <span>${pricePerNight * totalNights}</span>
         </div>
       </div>
 
       {/* SUBMIT */}
-      {/* <ButtonPrimary href={'/checkout'}>Reserve</ButtonPrimary> */}
+      <Button type='submit' onClick={handleSubmit}>
+        {user ? (
+          <>
+            Book Now
+            <span className='sr-only'>Book Now</span>
+          </>
+        ) : (
+          <>
+            Sign in to Book
+            <span className='sr-only'> Sign in to Book</span>
+          </>
+        )}
+      </Button>
     </>
   )
 }
